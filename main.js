698de23018db1118b1ef8013e9b6d29a0a441e69
Darrel Toledo <darreltoledo@icloud.com>
2022-05-10 22:03:33 +0100

    form validation method

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 let form = document.getElementById("form");
+let textInput = document.getElementById("textInput")
 
-form.addEventListener('submit', (e)=>{
+form.addEventListener("submit", (e)=>{
   e.preventDefault();
 });
+
+let formValidation = ()=> {
+  if(textInput.value === ""){
+
+  }
+  else {
+
+  }
+}
