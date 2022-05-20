49f6c9a8adb23131f36d9c6260587250b1001226
Darrel Toledo <darreltoledo@icloud.com>
2022-05-20 22:09:37 +0100

    delete task function and passed it to the icon

--- main.js
+++ main.js
@@ -48,11 +48,15 @@ let createTasks = () => {
   <p>${data.description}</p>
   <span class="options">
     <i class="fas fa-edit"></i>
-    <i class="fas fa-trash-alt"></i>
+    <i onClick ="deleteTask(this)" class="fas fa-trash-alt"></i>
   </span>
 </div>`;
 
 resetForm();
+};
+
+let deleteTask = ()=>{
+
 };
 
 let resetForm = () => {
