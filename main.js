4ce875ed5f18ab760dd8429c453787bc8335c943
Darrel Toledo <darreltoledo@icloud.com>
2022-05-31 22:24:08 +0100

    local storage will update the deleted tasks

--- main.js
+++ main.js
@@ -62,6 +62,7 @@ resetForm();
 let deleteTask = (e)=>{
   e.parentElement.parentElement.remove();
   data.splice(e.parentElement.parentElement.id, 1);
+  localStorage.setItem("data", JSON.stringify(data));
 };
 
 let editTask = (e)=> {
