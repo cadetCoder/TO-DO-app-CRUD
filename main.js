261c232545a2c1fb677af3cb35cf1118c7518856
Darrel Toledo <darreltoledo@icloud.com>
2022-06-02 22:10:59 +0100

    debug thick arrow on delete task method

--- main.js
+++ main.js
@@ -59,7 +59,7 @@ let createTasks = () => {
 resetForm();
 };
 
-let deleteTask = (e)=>{
+let deleteTask = (e) => {
   e.parentElement.parentElement.remove();
   data.splice(e.parentElement.parentElement.id, 1);
   localStorage.setItem("data", JSON.stringify(data));
